d033d4193455f8175fa580ecb2f0d055bd83f85e
INT\raheeh <hajira.raheem@msg-global.com>
2020-09-29 18:57:40 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'; // from node_modules
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my react-app-skeleton</p>, document.getElementById('app'));
